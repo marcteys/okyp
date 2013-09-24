2ae1a427dd79c282b42a8605427ff7b122224c70
marcteys <marc.teys@gmail.com>
2013-09-24 21:03:49 +0100

    adding leap motion lib and test

--- js/main.js
+++ js/main.js
@@ -11,3 +11,58 @@
 
 
 
+   console.log('CHECK');
+      var SCENE_SIZE = 1000, camera, scene, renderer, stats, clock, geometry, material, mesh,controller, hands= [];
+
+      var init = function () {
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, SCENE_SIZE/1000, SCENE_SIZE );
+        camera.position.z = SCENE_SIZE/2;
+        scene = new THREE.Scene();
+        geometry = new THREE.SphereGeometry(SCENE_SIZE/20,4,4);
+        material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth:  1} );
+        for(var i = 0; i<2; i ++){
+          var hand = new THREE.Mesh( geometry, material );
+          hand.position.x = SCENE_SIZE * 1000;
+          hand.fingers = [];
+          for (var j = 0; j<5; j++){
+            var finger = new THREE.Mesh(geometry,material);
+            finger.position.x = SCENE_SIZE * 1000;
+            scene.add(finger);
+            hand.fingers.push(finger);
+          }
+          scene.add( hand );
+          hands.push( hand );
+        }
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        Leap.loop(function(frame){
+          for(var i = 0; i <2; i++){
+            if(frame.hands[i]){
+              hands[i].position = leapToScene(frame.hands[i].palmPosition)
+              for(var j = 0; j <5; j++){
+                if(frame.hands[i].fingers[j]){
+                  hands[i].fingers[j].position = leapToScene(frame.hands[i].fingers[j].tipPosition)
+                }else{
+                  hands[i].fingers[j].position.x = SCENE_SIZE * 1000
+                }
+              }
+            }else{
+              hands[i].position.x = SCENE_SIZE *1000
+            }
+          }
+        });
+      }
+      var animate = function () {
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera );
+      }
+      init();
+      animate();
+      function leapToScene(leapPosition){
+        var x = (leapPosition[0]/300)*SCENE_SIZE
+        var y = (((leapPosition[1])-200)/300)*SCENE_SIZE
+        var z = (leapPosition[2]/300)*SCENE_SIZE
+        var toReturn = new THREE.Vector3(x,y,z)
+        return toReturn
+      }
