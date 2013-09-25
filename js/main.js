3c5caac1350acc7c38b9198ae9c56c8da4a37d68
marcteys <marc.teys@gmail.com>
2013-09-25 22:31:43 +0100

    test ajout de grille

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ window.requestAnimFrame = (function() {
 			window.setTimeout(callback, 1000 / 60);
 	};
 })();
-
+/*
 //global var
 var container = document.getElementById('container');
 
@@ -70,7 +70,7 @@ container.appendChild(renderer.domElement);
 // 
 // 
 // 
-
+/*
 var controller = new Leap.Controller({
 	enableGestures: true
 });
@@ -91,6 +91,19 @@ controller.on('frame', function(data) {
 
 
 
+        var fingers = [];
+
+    for( var j = 0; j < fingers.length; j++ ){
+
+          var finger = new THREE.Mesh(geometry,spherematerial);
+            finger.position.x = 0
+            scene.add(finger);
+            fingers.push(finger);
+    }
+
+
+
+
 	for (var i = 0; i < frame.gestures.length; i++) {
 
 		var gesture = frame.gestures[0];
@@ -103,6 +116,7 @@ controller.on('frame', function(data) {
 
 			case "keyTap":
 				sphere.position.x = gesture.position[0];
+        console.log('tap'); 
 				break;
 
 		}
@@ -146,11 +160,110 @@ function leapToScene(leapPos) {
 })();
 
 
+*/
 
-/* Leap fonctione
+// MAIN
+
+// standard global variables
+var container, scene, camera, renderer, controls, stats;
+var clock = new THREE.Clock();
+
+// custom global variables
+var mesh;
+      var animate = function () {
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera );
+      }
+init();
+animate();
+
+// FUNCTIONS    
+function init() 
+{
+  // SCENE
+  scene = new THREE.Scene();
+  // CAMERA
+  var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+  var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+  camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+  scene.add(camera);
+  camera.position.set(SCREEN_WIDTH/2,150,400);
+  camera.lookAt(scene.position);  
+  // RENDERER
+    renderer = new THREE.WebGLRenderer( {antialias:true} );
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  container = document.getElementById( 'container' );
+  container.appendChild( renderer.domElement );
+  // EVENTS
+  // CONTROLS
+  // STATS
+  // LIGHT
+  var light = new THREE.PointLight(0xffffff);
+  light.position.set(100,250,100);
+  scene.add(light);
+  /*
+  // FLOOR
+  var floorTexture = new THREE.ImageUtils.loadTexture( 'images/checkerboard.jpg' );
+  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
+  floorTexture.repeat.set( 10, 10 );
+  var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+  var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.position.y = -0.5;
+  floor.rotation.x = Math.PI / 2;
+  scene.add(floor);
+  */
+  // SKYBOX
+  var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
+  var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x9999ff, side: THREE.BackSide } );
+  var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+  scene.add(skyBox);
+  
+  ////////////
+  // CUSTOM //
+  ////////////
+  
+  var geometry = new THREE.SphereGeometry( 30, 32, 16 );
+  var material = new THREE.MeshLambertMaterial( { color: 0x000088 } );
+  mesh = new THREE.Mesh( geometry, material );
+  mesh.position.set(40,40,40);
+  scene.add(mesh);
+  
+  var axes = new THREE.AxisHelper(50);
+  axes.position = mesh.position;
+  scene.add(axes);
+  
+  var gridXZ = new THREE.GridHelper(100, 10);
+  gridXZ.setColors( new THREE.Color(0x006600), new THREE.Color(0x006600) );
+  gridXZ.position.set( 100,0,100 );
+  scene.add(gridXZ);
+  
+  var gridXY = new THREE.GridHelper(100, 10);
+  gridXY.position.set( 100,100,0 );
+  gridXY.rotation.x = Math.PI/2;
+  gridXY.setColors( new THREE.Color(0x000066), new THREE.Color(0x000066) );
+  scene.add(gridXY);
+
+  var gridYZ = new THREE.GridHelper(100, 10);
+  gridYZ.position.set( 0,100,100 );
+  gridYZ.rotation.z = Math.PI/2;
+  gridYZ.setColors( new THREE.Color(0x660000), new THREE.Color(0x660000) );
+  scene.add(gridYZ);
+  
+  // direction (normalized), origin, length, color(hex)
+  var origin = new THREE.Vector3(50,100,50);
+  var terminus  = new THREE.Vector3(75,75,75);
+  var direction = new THREE.Vector3().subVectors(terminus, origin).normalize();
+  var arrow = new THREE.ArrowHelper(direction, origin, 50, 0x884400);
+  scene.add(arrow);
+  
+  
+  
+}
 
 
-   console.log('CHECK');
+/*
+
       var SCENE_SIZE = 1000, camera, scene, renderer, stats, clock, geometry, material, mesh,controller, hands= [];
 
       var init = function () {
