256b7463621ee8e90f06ca803dd251e5a7835743
Marc Teyssier <marc.teys@gmail.com>
2013-09-26 17:40:50 +0100

    lights and materials

--- js/main.js
+++ js/main.js
@@ -33,7 +33,9 @@ exemple ;isc control transform
       var cross;
 
       var clock, geometry, material, mesh,controller, hands= [];
+
 var SCENE_SIZE = 1000;
+
       init();
       animate();
 
@@ -42,6 +44,11 @@ var SCENE_SIZE = 1000;
         camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
         camera.position.z = 2000;
         camera.position.y = 1000;
+
+                scene = new THREE.Scene();
+
+camera.lookAt(scene.position);
+
         controls = new THREE.TrackballControls( camera );
 
         controls.rotateSpeed = 1.0;
@@ -60,7 +67,6 @@ var SCENE_SIZE = 1000;
 
         // world
 
-        scene = new THREE.Scene();
 
 
  geometry = new THREE.SphereGeometry(SCENE_SIZE/20,4,4);
@@ -79,7 +85,29 @@ var SCENE_SIZE = 1000;
           hands.push( hand );
         }
 
+  
 
+        var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+        var fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+
+
+
+
+
+
+var matBlue = new THREE.MeshPhongMaterial({color: 0x025D8C});
+
+var matOrange = new THREE.MeshPhongMaterial({color: 0xCC3300});
+
+var matRed = new THREE.MeshPhongMaterial({color: 0xC6010A});
+matRed.side = THREE.DoubleSide;
+matBlue.side = THREE.DoubleSide;
+
+var sphermesh = new THREE.Mesh(new THREE.SphereGeometry( 150,32,32 ), matRed);;
+var sphermesh2 = new THREE.Mesh(new THREE.SphereGeometry( 150,32,32 ), matBlue);;
+sphermesh2.position.x = 450;
+scene.add(sphermesh);
+scene.add(sphermesh2);
 
 var geom = new THREE.Geometry(); 
 var v1 = new THREE.Vector3(0,0,0);
@@ -93,7 +121,7 @@ geom.vertices.push(v3);
 geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
 geom.computeFaceNormals();
 
-var object = new THREE.Mesh( geom, new THREE.MeshPhongMaterial() );
+var object = new THREE.Mesh( geom, matBlue );
 
 object.position.z = -100;//move a bit back - size of 500 is a bit big
 object.rotation.y = -Math.PI * .5;//triangle is pointing in depth, rotate it -90 degrees on Y
@@ -108,9 +136,9 @@ scene.add(object);
 
 
 var geom2 = new THREE.Geometry(); 
-var v1 = new THREE.Vector3(0,0,0);
-var v2 = new THREE.Vector3(0,400,10);
-var v3 = new THREE.Vector3(60,100,600);
+var v1 = new THREE.Vector3(0,500,0);
+var v2 = new THREE.Vector3(0,500,500);
+var v3 = new THREE.Vector3(-300,700,600);
 
 geom2.vertices.push(v1);
 geom2.vertices.push(v2);
@@ -119,7 +147,7 @@ geom2.vertices.push(v3);
 geom2.faces.push( new THREE.Face3( 0, 1, 2 ) );
 geom2.computeFaceNormals();
 
-var object2 = new THREE.Mesh( geom2, new THREE.MeshPhongMaterial() );
+var object2 = new THREE.Mesh( geom2, matBlue );
 
 object2.position.z = -100;//move a bit back - size of 500 is a bit big
 object2.rotation.y = -Math.PI * .5;//triangle is pointing in depth, rotate it -90 degrees on Y
@@ -127,11 +155,24 @@ object2.rotation.y = -Math.PI * .5;//triangle is pointing in depth, rotate it -9
 scene.add(object2);
 
 
+
+
+
+
+// Drawing grid
+
 addGrid(1000,100,0x000000,0.2);
 
-  // LIGHTS
 
-        hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+  // LIGHTS
+  // 
+  
+  // nice hipster effect var ambientLight = new THREE.AmbientLight(0x000044);
+  var ambientLight = new THREE.AmbientLight(0x222233);
+      scene.add(ambientLight);
+
+
+        hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.7 );
         hemiLight.color.setHSL( 0.6, 1, 0.6 );
         hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
         hemiLight.position.set( 0, 500, 0 );
@@ -139,33 +180,11 @@ addGrid(1000,100,0x000000,0.2);
 
         //
 
-        dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-        dirLight.color.setHSL( 0.1, 1, 0.95 );
+        dirLight = new THREE.DirectionalLight( 0xffffff, .8 );
+        dirLight.color.setHSL( 1, 1, 0.95 );
         dirLight.position.set( -1, 1.75, 1 );
         dirLight.position.multiplyScalar( 50 );
         scene.add( dirLight );
-
-        dirLight.castShadow = true;
-
-        dirLight.shadowMapWidth = 2048;
-        dirLight.shadowMapHeight = 2048;
-
-        var d = 50;
-
-        dirLight.shadowCameraLeft = -d;
-        dirLight.shadowCameraRight = d;
-        dirLight.shadowCameraTop = d;
-        dirLight.shadowCameraBottom = -d;
-
-        dirLight.shadowCameraFar = 3500;
-        dirLight.shadowBias = -0.0001;
-        dirLight.shadowDarkness = 0.35;
-        //dirLight.shadowCameraVisible = true;
-
-
-
-
-
 
 
 
