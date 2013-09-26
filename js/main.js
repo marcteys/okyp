b665618793fc3ce3e2fa386862cf713f64f3406f
Marc Teyssier <marc.teys@gmail.com>
2013-09-26 15:59:27 +0100

    adding lights

--- js/main.js
+++ js/main.js
@@ -55,6 +55,89 @@ window.requestAnimFrame = (function() {
           hands.push( hand );
         }
 
+        // LIGHTS
+
+        hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+        hemiLight.color.setHSL( 0.6, 1, 0.6 );
+        hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+        hemiLight.position.set( 0, 500, 0 );
+        scene.add( hemiLight );
+
+        //
+
+        dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+        dirLight.color.setHSL( 0.1, 1, 0.95 );
+        dirLight.position.set( -1, 1.75, 1 );
+        dirLight.position.multiplyScalar( 50 );
+        scene.add( dirLight );
+
+        dirLight.castShadow = true;
+
+        dirLight.shadowMapWidth = 2048;
+        dirLight.shadowMapHeight = 2048;
+
+        var d = 50;
+
+        dirLight.shadowCameraLeft = -d;
+        dirLight.shadowCameraRight = d;
+        dirLight.shadowCameraTop = d;
+        dirLight.shadowCameraBottom = -d;
+
+        dirLight.shadowCameraFar = 3500;
+        dirLight.shadowBias = -0.0001;
+        dirLight.shadowDarkness = 0.35;
+        //dirLight.shadowCameraVisible = true;
+
+
+var geom = new THREE.Geometry(); 
+var v1 = new THREE.Vector3(0,0,0);
+var v2 = new THREE.Vector3(0,500,0);
+var v3 = new THREE.Vector3(0,500,500);
+
+geom.vertices.push(v1);
+geom.vertices.push(v2);
+geom.vertices.push(v3);
+
+geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
+geom.computeFaceNormals();
+
+var object = new THREE.Mesh( geom, new THREE.MeshPhongMaterial() );
+
+object.position.z = -100;//move a bit back - size of 500 is a bit big
+object.rotation.y = -Math.PI * .5;//triangle is pointing in depth, rotate it -90 degrees on Y
+
+scene.add(object);
+
+
+
+
+
+
+
+
+var geom2 = new THREE.Geometry(); 
+var v1 = new THREE.Vector3(0,0,0);
+var v2 = new THREE.Vector3(0,400,10);
+var v3 = new THREE.Vector3(60,100,600);
+
+geom2.vertices.push(v1);
+geom2.vertices.push(v2);
+geom2.vertices.push(v3);
+
+geom2.faces.push( new THREE.Face3( 0, 1, 2 ) );
+geom2.computeFaceNormals();
+
+var object2 = new THREE.Mesh( geom2, new THREE.MeshPhongMaterial() );
+
+object2.position.z = -100;//move a bit back - size of 500 is a bit big
+object2.rotation.y = -Math.PI * .5;//triangle is pointing in depth, rotate it -90 degrees on Y
+
+scene.add(object2);
+
+
+
+
+
 
 
 
