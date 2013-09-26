9495d8afca6b2767b5b614e8ebc08507694fd58b
Marc Teyssier <marc.teys@gmail.com>
2013-09-26 16:13:14 +0100

    implementation good design practice / optimisation

--- js/main.js
+++ js/main.js
@@ -17,8 +17,130 @@ deux doigts pour modéliser
 
 
 exemple rotation : voxel paint
+exemple ;isc control transform
+
 
 */
+
+
+
+
+
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+      var container, stats;
+
+      var camera, controls, scene, renderer;
+
+      var cross;
+
+      init();
+      animate();
+
+      function init() {
+
+        camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+        camera.position.z = 500;
+
+        controls = new THREE.OrbitControls( camera );
+        controls.addEventListener( 'change', render );
+
+        scene = new THREE.Scene();
+        scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+        // world
+
+        var geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
+        var material =  new THREE.MeshLambertMaterial( { color:0xffffff, shading: THREE.FlatShading } );
+
+        for ( var i = 0; i < 500; i ++ ) {
+
+          var mesh = new THREE.Mesh( geometry, material );
+          mesh.position.x = ( Math.random() - 0.5 ) * 1000;
+          mesh.position.y = ( Math.random() - 0.5 ) * 1000;
+          mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+          mesh.updateMatrix();
+          mesh.matrixAutoUpdate = false;
+          scene.add( mesh );
+
+        }
+
+
+        // lights
+
+        light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 1, 1, 1 );
+        scene.add( light );
+
+        light = new THREE.DirectionalLight( 0x002288 );
+        light.position.set( -1, -1, -1 );
+        scene.add( light );
+
+        light = new THREE.AmbientLight( 0x222222 );
+        scene.add( light );
+
+
+        // renderer
+
+        renderer = new THREE.WebGLRenderer( { antialias: false } );
+        renderer.setClearColor( scene.fog.color, 1 );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        container = document.getElementById( 'container' );
+        container.appendChild( renderer.domElement );
+
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.top = '0px';
+        stats.domElement.style.zIndex = 100;
+        container.appendChild( stats.domElement );
+
+        //
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+      }
+
+      function onWindowResize() {
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        render();
+
+      }
+
+      function animate() {
+
+        requestAnimationFrame( animate );
+        controls.update();
+
+      }
+
+      function render() {
+
+        renderer.render( scene, camera );
+        stats.update();
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 window.requestAnimFrame = (function() {
 	return window.requestAnimationFrame ||
 		window.webkitRequestAnimationFrame ||
@@ -30,6 +152,9 @@ window.requestAnimFrame = (function() {
 
 
 
+
+var controls;
+
       var SCENE_SIZE = 1000, camera, scene, renderer, stats, clock, geometry, material, mesh,controller, hands= [];
 
       var init = function () {
@@ -37,7 +162,28 @@ window.requestAnimFrame = (function() {
         camera.position.z = SCENE_SIZE*2;
         camera.position.y = 1000;
         scene = new THREE.Scene();
-                camera.lookAt( scene.position );
+
+
+
+
+ controls = new THREE.TrackballControls( camera );
+
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
+
+        controls.noZoom = false;
+        controls.noPan = false;
+
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+
+        controls.keys = [ 65, 83, 68 ];
+
+        controls.addEventListener( 'change', render );
+
+
+               // GOOD camera.lookAt( scene.position );
 
         geometry = new THREE.SphereGeometry(SCENE_SIZE/20,4,4);
         material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth:  1} );
@@ -139,8 +285,6 @@ scene.add(object2);
 
 
 
-
-
         // grid
         //         var size = 500, step = 50;
 
@@ -213,4 +357,4 @@ function addGrid(size,step,color,opacity) {
         scene.add( plane );
 
 
-}
+}*/
